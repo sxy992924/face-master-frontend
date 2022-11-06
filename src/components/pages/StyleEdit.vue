29ee47cbe687da00f02a19360de786abc880f4ef
魏子涵 <muzhenweizh@gmail.com>
2022-11-07 01:25:50 +0800

    feat: superRes & styleEdit pages

--- src/components/pages/StyleEdit.vue
+++ src/components/pages/StyleEdit.vue
@@ -1,19 +1,69 @@
 <script setup>
-import low_stargan_0 from '@/assets/low_stargan_0.jpg'
+// stargan
+import p_000007_0 from '@/assets/stargan/000007_0.jpg'
+import p_000007_16 from '@/assets/stargan/000007_16.jpg'
+import p_000007_17 from '@/assets/stargan/000007_17.jpg'
+import p_000007_18 from '@/assets/stargan/000007_18.jpg'
+import p_000007_19 from '@/assets/stargan/000007_19.jpg'
+import p_000007_20 from '@/assets/stargan/000007_20.jpg'
+// stgan
+import p_182643_0 from '@/assets/stgan/182643_0.jpg'
+import p_182643_1 from '@/assets/stgan/182643_1.png'
+import p_182643_2 from '@/assets/stgan/182643_2.png'
+// faceapp
+import p_000009_0 from '@/assets/faceapp/000009_0.jpg'
+import p_000009_1 from '@/assets/faceapp/000009_1.jpg'
+import p_000009_2 from '@/assets/faceapp/000009_2.jpg'
 </script>
 
 <template>
   <div
-    class="grid grid-cols-[3rem,1fr,1fr,1fr,1fr,1fr,1fr] grid-rows-[2rem,1fr,1fr,1fr,1fr,1fr,1fr] gap-4"
+    class="grid grid-cols-[3rem,1fr,1fr,1fr,1fr,1fr,1fr] grid-rows-[1rem,1fr] gap-4"
   >
     <!-- first line -->
     <div />
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      原图
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      戴眼镜
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      山羊胡
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      灰发
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      浓妆
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      高颧骨
+    </n-tag>
     <!-- second line -->
     <span
       class="justify-self-center self-center"
@@ -23,34 +73,64 @@ import low_stargan_0 from '@/assets/low_stargan_0.jpg'
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_0"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_16"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_17"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_18"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_19"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000007_20"
     />
-    <!-- third line -->
+  </div>
+  <div
+    class="mt-12 grid grid-cols-[3rem,1fr,1fr,1fr,1fr,1fr,1fr] grid-rows-[1rem,1fr,1fr] gap-4"
+  >
+    <!-- first line -->
+    <div />
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      原图
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      年轻/老年
+    </n-tag>
+    <n-tag
+      :bordered="false"
+      type="success"
+      class="justify-self-center self-center"
+    >
+      男性/女性
+    </n-tag>
+    <div />
+    <div />
+    <div />
+    <!-- second line -->
     <span
       class="justify-self-center self-center"
     >
@@ -59,69 +139,45 @@ import low_stargan_0 from '@/assets/low_stargan_0.jpg'
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000009_0"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000009_1"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_000009_2"
     />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
-    <!-- fourth line -->
+    <div />
+    <div />
+    <div />
+    <!-- third line -->
     <span
       class="justify-self-center self-center"
     >
-      FaceApp
+      FaceAPP
     </span>
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_182643_0"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_182643_1"
     />
     <n-image
       class="justify-center"
       width="112"
-      :src="low_stargan_0"
+      :src="p_182643_2"
     />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="112"
-      :src="low_stargan_0"
-    />
+    <div />
+    <div />
+    <div />
   </div>
 </template>
 
--- src/components/pages/SuperRes.vue
+++ src/components/pages/SuperRes.vue
@@ -1,127 +1,130 @@
 <script setup>
-import high_stargan_0 from '@/assets/high_stargan_0.jpg'
+import low_10 from '@/assets/low_10.jpg'
+import low_11 from '@/assets/low_11.jpg'
+import low_12 from '@/assets/low_12.jpg'
+import low_13 from '@/assets/low_13.jpg'
+import low_14 from '@/assets/low_14.jpg'
+import low_15 from '@/assets/low_15.jpg'
+import low_16 from '@/assets/low_16.jpg'
+import low_17 from '@/assets/low_17.jpg'
+import low_18 from '@/assets/low_18.jpg'
+import low_19 from '@/assets/low_19.jpg'
+import high_10 from '@/assets/high_10.jpg'
+import high_11 from '@/assets/high_11.jpg'
+import high_12 from '@/assets/high_12.jpg'
+import high_13 from '@/assets/high_13.jpg'
+import high_14 from '@/assets/high_14.jpg'
+import high_15 from '@/assets/high_15.jpg'
+import high_16 from '@/assets/high_16.jpg'
+import high_17 from '@/assets/high_17.jpg'
+import high_18 from '@/assets/high_18.jpg'
+import high_19 from '@/assets/high_19.jpg'
+
+const selectVal = ref(low_10)
+const highImage = ref(null)
+const showHighImage = ref(false)
+const options = [
+  {
+    label: 'low_10',
+    value: low_10,
+  },
+  {
+    label: 'low_11',
+    value: low_11,
+  },
+  {
+    label: 'low_12',
+    value: low_12,
+  },
+  {
+    label: 'low_13',
+    value: low_13,
+  },
+  {
+    label: 'low_14',
+    value: low_14,
+  },
+  {
+    label: 'low_15',
+    value: low_15,
+  },
+  {
+    label: 'low_16',
+    value: low_16,
+  },
+  {
+    label: 'low_17',
+    value: low_17,
+  },
+  {
+    label: 'low_18',
+    value: low_18,
+  },
+  {
+    label: 'low_19',
+    value: low_19,
+  },
+]
+
+const lowToHighMap = new Map([
+  [low_10, high_10],
+  [low_11, high_11],
+  [low_12, high_12],
+  [low_13, high_13],
+  [low_14, high_14],
+  [low_15, high_15],
+  [low_16, high_16],
+  [low_17, high_17],
+  [low_18, high_18],
+  [low_19, high_19],
+])
+
+function handleSelectValueUpdate() {
+  showHighImage.value = false
+}
+function handleSwinIR() {
+  highImage.value = lowToHighMap.get(selectVal.value)
+  showHighImage.value = true
+}
 </script>
 
 <template>
   <div
-    class="grid grid-cols-[3rem,1fr,1fr,1fr,1fr,1fr,1fr] grid-rows-[2rem,1fr,1fr,1fr,1fr,1fr,1fr] gap-4"
+    class="flex justify-center items-center gap-6 bg-black/5 rounded-xl p-6"
   >
-    <!-- first line -->
-    <div />
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <span class="justify-self-center">原图</span>
-    <!-- second line -->
-    <span
-      class="justify-self-center self-center"
+    <div class="flex flex-col gap-2">
+      <n-image
+        class="justify-center"
+        width="112"
+        :src="selectVal"
+      />
+      <n-select
+        v-model:value="selectVal"
+        :options="options"
+        @update:value="handleSelectValueUpdate"
+      />
+    </div>
+    <n-button
+      strong
+      secondary
+      round
+      type="primary"
+      icon-placement="right"
+      @click="handleSwinIR"
     >
-      StarGAN
-    </span>
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <!-- third line -->
-    <span
-      class="justify-self-center self-center"
-    >
-      STGAN
-    </span>
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <!-- fourth line -->
-    <span
-      class="justify-self-center self-center"
-    >
-      FaceApp
-    </span>
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
-    <n-image
-      class="justify-center"
-      width="448"
-      :src="high_stargan_0"
-    />
+      <template #icon>
+        <i-material-symbols-arrow-right-alt />
+      </template>
+      SwinIR
+    </n-button>
+    <div class="w-[448px] h-[448px] border border-black">
+      <n-image
+        v-show="showHighImage"
+        class="justify-center"
+        width="448"
+        :src="highImage"
+      />
+    </div>
   </div>
 </template>
 
